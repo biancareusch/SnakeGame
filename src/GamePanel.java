b188345fdfb6628439b5a20e67f0a7a240f3b202
Bianca Reusch <reusch.bianca@gmail.com>
2020-11-09 14:47:23 -0600

    startGame method finished

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -15,40 +15,67 @@ public class GamePanel extends JPanel implements ActionListener {
 
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
 
-    GamePanel(){
-
-    }
-    public void startGame(){
-
-    }
-
-    public void paintComponent(Graphics g){
-
-    }
-    public void draw(Graphics g){
-
-    }
-    public void move(){
-
-    }
-    public void checkApple(){
-
-    }
-    public void checkCollisions(){
-
-    }
-    public void gameOveR(Graphics g){
-
+    GamePanel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
 
-@Override
-    public void actionPerformed(ActionEvent e){
+    public void startGame() {
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    public void paintComponent(Graphics g) {
 
     }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void newApple() {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOveR(Graphics g) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
     public class MyKeyAdapter extends KeyAdapter {
         @Override
-        public void keyPressed(KeyEvent e){
+        public void keyPressed(KeyEvent e) {
 
         }
 
